09fab9c19bcfe67379fbff82daa436ccf602321e
MassiGy <ghernaoutmassi@gmail.com>
2022-10-14 15:08:55 +0200

    removed wrong macros

--- headers/tries.h
+++ headers/tries.h
@@ -3,14 +3,8 @@
 #include "./headers/stringManipulation.h"
 #include "./headers/singleLinkedLists.h"
 
-#ifndef FILE_LINE_COUNT
 #define FILE_LINE_COUNT 10000
-#endif
-
-#ifndef WORD_PER_LINE
 #define WORD_PER_LINE 1
-#endif
-
 #define ALPHABET_LETTER_COUNT 26
 #define ASCII_ORD_OFFSET 97
 
